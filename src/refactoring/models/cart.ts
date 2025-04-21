30631bf2818fe679fecf0c23f29760ee27799942
me-hana <hayonleee@gmail.com>
2025-04-21 16:32:32 +0900

    refactor: CartPage 컴포넌트가 온전히 렌더링에만 관여할 수 있도록 계산 함수 추출

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -1,4 +1,4 @@
-import { CartItem, Coupon } from "../../types";
+import { CartItem, Coupon, Product } from "../../types";
 
 export const calculateItemTotal = (item: CartItem) => {
   return 0;
@@ -8,9 +8,28 @@ export const getMaxApplicableDiscount = (item: CartItem) => {
   return 0;
 };
 
+// getMaxApplicableDiscount은 getMaxDiscount, getAppliedDiscount를 합쳐서 만들면 되나?
+export const getMaxDiscount = (
+  discounts: { quantity: number; rate: number }[],
+) => {
+  return discounts.reduce((max, discount) => Math.max(max, discount.rate), 0);
+};
+
+export const getAppliedDiscount = (item: CartItem) => {
+  const { discounts } = item.product;
+  const { quantity } = item;
+  let appliedDiscount = 0;
+  for (const discount of discounts) {
+    if (quantity >= discount.quantity) {
+      appliedDiscount = Math.max(appliedDiscount, discount.rate);
+    }
+  }
+  return appliedDiscount;
+};
+
 export const calculateCartTotal = (
   cart: CartItem[],
-  selectedCoupon: Coupon | null
+  selectedCoupon: Coupon | null,
 ) => {
   return {
     totalBeforeDiscount: 0,
@@ -22,7 +41,13 @@ export const calculateCartTotal = (
 export const updateCartItemQuantity = (
   cart: CartItem[],
   productId: string,
-  newQuantity: number
+  newQuantity: number,
 ): CartItem[] => {
   return [];
+};
+
+// 엄밀히 말하면 Cart보다는 재고랑 관련된 계산 함수
+export const getRemainingStock = (product: Product, cart: CartItem[]) => {
+  const cartItem = cart.find((item) => item.product.id === product.id);
+  return product.stock - (cartItem?.quantity || 0);
 };
