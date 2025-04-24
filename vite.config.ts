92175660af40db398c1b83172ffb688f3363aac5
me-hana <hayonleee@gmail.com>
2025-04-24 23:52:29 +0900

    chore: 배포 설정하기

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+const isCI = process.env.CI === "true";
 
 export default mergeConfig(
   defineConfig({
+    base: isCI ? "" : "/front_5th_chapter2-2/",
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, "index.refactoring.html"),
+          origin: path.resolve(__dirname, "index.origin.html"),
+        },
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
-  })
-)
+  }),
+);
